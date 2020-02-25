328ca46314dda67e274939804c0865dcde89424f
rajithavr <rajitha.vr@gmail.com>
2020-02-25 13:36:13 -0500

    Added method getName

--- Home.java
+++ Home.java
@@ -2,6 +2,9 @@ public class Home{
     public static void main(String args[]){
         system.out.println("Hello World");
         system.out.println("Next Line");
+        public void getName(){
+            
+        }
     }
 
 }
